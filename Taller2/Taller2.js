2dff1b7605eb1d49ccd724456c468a4e35d12e40
Jordy <Jarrisonumpaque232@outlook.es>
2023-05-16 17:01:05 -0500

    intento de taller aun en proceso

--- Taller2/Taller2.js
+++ Taller2/Taller2.js
@@ -1,18 +1,46 @@
-const Array = []
 
 function MayorArray(Array) {
-   if (Array == 0) {
-        console.log('Null, no hay numeros')
-   }
-   if (Array.length == 1) {
-        console.log('EL unico elemento que hay es ' + Array[0])
-   }
 
-//    if (Array.length == 2 || Array.length > 2) { 
-
-//         let NumMayor = Math.max(...Array)
-//         console.log('el numero mayo es ' + NumMayor)
-//     }
-// Ya hubiera acabado aqui iwi, pero lei los requisitos y me toco ahora hacerlo con for T-T
+ //si no hay numeros en el array   
+   if (Array.length === 0) {
+        console.log('Null');
 }
-MayorArray([1,3,5,67,7,2])
+   if (Array.length === 1) {
+    console.log('EL unico elemento que hay es ' + Array[0])
+}
+
+//Encuentra el numero mayor
+let NumeroMayor = Array[0]
+for (let i = 1; i < Array.length; i++) {
+    if (Array[i] > NumeroMayor) {
+        NumeroMayor = Array[i]
+    }    
+    console.log('el numero mayor es: ' + NumeroMayor)
+}
+// imprimer los elementos del array
+let Elemento = 0
+while (Elemento < Array.length) {
+    console.log('estos son los elementos que hay: '+Array[Elemento])
+    Elemento++
+}
+
+//Busca el impar
+
+let impar = 0
+let elementoArray = 0
+
+do {
+    if (Array[elementoArray]% 2 !==0) {
+        impar++
+    }
+    elementoArray++
+    
+} while (elementoArray < Array.length);
+    console.log('impar: ' + impar)
+
+
+}
+
+
+const Array = [50,70,30]
+const resultado = MayorArray(Array)
